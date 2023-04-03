ec2727f63ee00e2cae4a291b03bfe701e2bae5f3
Nassim Marhari <92116345+nmarhari@users.noreply.github.com>
2023-04-03 16:36:59 -0400

    Update producer.cpp

--- cpp/producer.cpp
+++ cpp/producer.cpp
@@ -18,8 +18,10 @@ const int SPACE = (SIZE * sizeof(int));
 
 int main() {
 
-	sem_t *mutex = sem_open("mutex", O_CREAT, 0666, 1);
-	sem_t *full = sem_open("mutex", O_CREAT, 0666, 2);
+
+	sem_t *mutex = sem_open("", O_CREAT, 0666, 1);
+	sem_t *full = sem_open("full", O_CREAT, 0666, 2);
+	printf("Opened semaphores.\n");
 
 	int table = shm_open("table", O_CREAT|O_RDWR, 0666);
 	
@@ -27,16 +29,20 @@ int main() {
 	
 	int *tbl = (int*)mmap(0, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, table, 0);
 
+	printf("Producer mapped address: %p\n", tbl);
 	
+	printf("1");
 	sem_wait(mutex);
-	printf("sending this to there");
-	printf("sender mapped address: %p\n", tbl);
-
+	printf("Producer called wait on MUTEX...");
+	
 	for (int i = 0; i < SIZE; i++) {
 		tbl[i] = i;
+		printf("Producer: %d\n", i);
 		sem_wait(full);
+		printf("Producer Full called wait...\n");
 	}
 	sem_post(mutex);
+	printf("Producer Mutex called post.\n");
 	
 	munmap(tbl, SPACE);
 	
@@ -44,6 +50,10 @@ int main() {
 	
 	sem_close(mutex);
 	sem_close(full);
+	
+	sem_destroy(mutex);
+	sem_destroy(full);
+	printf("Both semaphores destroyed.\n");
 
 
 	return 0;
