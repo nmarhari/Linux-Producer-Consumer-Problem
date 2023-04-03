ba1915d3d920a550487ce654d67bcb443b8edfe1
Nassim Marhari <92116345+nmarhari@users.noreply.github.com>
2023-04-03 17:11:33 -0400

    Update consumer.cpp

--- cpp/consumer.cpp
+++ cpp/consumer.cpp
@@ -13,44 +13,51 @@
 #include <stdio.h>
 #include <string.h>
 
-const int SIZE = 2048;
+const int SIZE = 2;
 const int SPACE = (SIZE * sizeof(int));
-const int ITEMS = 2;
 
 int main() {
 
 	sem_t *mutex = sem_open("mutex", O_CREAT, 0666, 1);
+	if (mutex == SEM_FAILED) { perror("error creating mutex"); exit(EXIT_FAILURE); }
 	sem_t *full = sem_open("full", O_CREAT,0666, 2);
+	if (full == SEM_FAILED) { perror("error creating full"); exit(EXIT_FAILURE); }
+	sem_t *empty = sem_open("empty", O_CREAT, 0666, SIZE);
+	if (empty == SEM_FAILED) { perror("error creating empty"); exit(EXIT_FAILURE); }
 
 	int shm = shm_open("table", O_RDONLY, 0666);
+
 	
 	int *tbl = (int*)mmap(0, SIZE, PROT_READ, MAP_SHARED, shm, 0);
-	printf("Receiver mapped address: %p\n", tbl);
+	printf("Consumer mapped address: %p\n", tbl);
 	
-	sem_wait(mutex);
-	printf("Consumer Mutex called wait...\n");
-		
-	for (int i = 0; i < ITEMS; ++i) {
+	printf("consumer started consuming data\n");
+	for (int i = 0; i < SIZE; i++) {
+		sem_wait(full);
+		printf("prod passed wait full");
+		if (sem_wait(full) == -1) { perror("error waiting on full"); exit(EXIT_FAILURE); }
+		sem_wait(mutex);
+		if (sem_wait(mutex) == -1) { perror("error waiting on mutex"); exit(EXIT_FAILURE); }
+		printf("inside for loop cons");
 		printf("Consumer: %d\n", tbl[i]);
+		sem_post(mutex);
+		sem_post(empty);
 	}
-	sem_post(full);
-	sem_post(full);
-	sem_post(mutex);
-	printf("Consumer Mutex called post.\n");
 		
 	
 	munmap(tbl, SPACE);
-	shm_unlink("table");
+	close(shm);
 
 	
 	sem_close(mutex);
 	sem_close(full);
+	sem_close(empty);
 	
 	sem_unlink("mutex");
 	sem_unlink("full");
-	
-	sem_destroy(mutex);
-	sem_destroy(full);
+	sem_unlink("empty");
 
+	printf("Consumer exiting.\n");
+	
 	return 0;
 }
