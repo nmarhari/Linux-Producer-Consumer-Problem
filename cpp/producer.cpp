fa6f5d797a676fca41cc13456f68c4efbd56c9e5
Nassim Marhari <92116345+nmarhari@users.noreply.github.com>
2023-04-03 16:48:52 -0400

    Update producer.cpp

--- cpp/producer.cpp
+++ cpp/producer.cpp
@@ -13,13 +13,14 @@
 #include <stdio.h>
 #include <string.h>
 
-const int SIZE = 2;
+const int SIZE = 2048;
 const int SPACE = (SIZE * sizeof(int));
+const int ITEMS = 2;
 
 int main() {
 
 
-	sem_t *mutex = sem_open("", O_CREAT, 0666, 1);
+	sem_t *mutex = sem_open("mutex", O_CREAT, 0666, 1);
 	sem_t *full = sem_open("full", O_CREAT, 0666, 2);
 	printf("Opened semaphores.\n");
 
@@ -35,7 +36,7 @@ int main() {
 	sem_wait(mutex);
 	printf("Producer called wait on MUTEX...");
 	
-	for (int i = 0; i < SIZE; i++) {
+	for (int i = 0; i < ITEMS; i++) {
 		tbl[i] = i;
 		printf("Producer: %d\n", i);
 		sem_wait(full);
@@ -45,11 +46,15 @@ int main() {
 	printf("Producer Mutex called post.\n");
 	
 	munmap(tbl, SPACE);
+	shm_unlink("table");
 	
 	close(table);
 	
 	sem_close(mutex);
 	sem_close(full);
+	
+	sem_unlink("mutex");
+	sem_unlink("full");
 	
 	sem_destroy(mutex);
 	sem_destroy(full);
