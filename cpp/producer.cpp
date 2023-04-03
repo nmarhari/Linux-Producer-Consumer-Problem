8808b94d2792cd700918086d9f536384a6a15392
Nassim Marhari <92116345+nmarhari@users.noreply.github.com>
2023-04-03 12:48:27 -0400

    Update producer.cpp

--- cpp/producer.cpp
+++ cpp/producer.cpp
@@ -1,7 +1,6 @@
 // Nassim Marhari
 // Linux Producer Consumer problem
 
-#include <iostream>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/shm.h>
@@ -14,24 +13,34 @@
 #include <stdio.h>
 #include <string.h>
 
-const int SIZE = 2048;
+const int SIZE = 2;
+const int SPACE = (SIZE * sizeof(int));
 
 int main() {
 
 	sem_t *mutex = sem_open("mutex", O_CREAT, 0666, 1);
-	sem_t *full = sem_open("mutex", O_CREAT, 0666, 0);
+	sem_t *full = sem_open("mutex", O_CREAT, 0666, 2);
 
 	int table = shm_open("table", O_CREAT|O_RDWR, 0666);
 	
 	ftruncate(table, SIZE);
 	
-	char *tbl = (char*)mmap(0, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, table, 0);
+	int *tbl = (int*)mmap(0, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, table, 0);
+
 	
-	const char *message0 = "hello";
-	int i = 0;
-	std::cout << "Sending message " << i << " to shared memory.\n";
-	sprintf(tbl, "%s", message0);
-	tbl += strlen(message0);
+	sem_wait(mutex);
+	printf("sending this to there");
+	printf("sender mapped address: %p\n", tbl);
+
+	for (int i = 0; i < SIZE; i++) {
+		tbl[i] = i;
+		sem_wait(full);
+	}
+	sem_post(mutex);
+	
+	munmap(tbl, SPACE);
+	
+	close(table);
 	
 	sem_close(mutex);
 	sem_close(full);
