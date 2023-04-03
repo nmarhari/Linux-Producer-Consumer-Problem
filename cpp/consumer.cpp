49040b5815d29092666814c2480edffee6fb8353
Nassim Marhari <92116345+nmarhari@users.noreply.github.com>
2023-04-03 16:37:13 -0400

    Update consumer.cpp

--- cpp/consumer.cpp
+++ cpp/consumer.cpp
@@ -16,25 +16,30 @@
 const int SIZE = 2;
 const int SPACE = (SIZE * sizeof(int));
 
+
 int main() {
 
 	sem_t *mutex = sem_open("mutex", O_CREAT, 0666, 1);
-	sem_t *full = sem_open("mutex", O_CREAT,0666, 0);
+	sem_t *full = sem_open("full", O_CREAT,0666, 2);
 
 	int shm = shm_open("table", O_RDONLY, 0666);
 	
 	int *tbl = (int*)mmap(0, SIZE, PROT_READ, MAP_SHARED, shm, 0);
-	printf("receiver mapped address: %p\n", tbl);
+	printf("Receiver mapped address: %p\n", tbl);
 	
 	sem_wait(mutex);
+	printf("Consumer Mutex called wait...\n");
+		
 	for (int i = 0; i < SIZE; ++i) {
-		printf("%d\n", tbl[i]);
+		printf("Consumer: %d\n", tbl[i]);
 		sem_post(full);
+		printf("Consumer Full called post.\n");
 	}
 	sem_post(mutex);
+	printf("Consumer Mutex called post.\n");
+		
 	
 	munmap(tbl, SPACE);
-	
 	
 	sem_close(mutex);
 	sem_close(full);
