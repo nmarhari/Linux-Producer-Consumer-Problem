8f31a3367a86689d38bfb67fd3bb873f2e190f83
Nassim Marhari <92116345+nmarhari@users.noreply.github.com>
2023-04-03 23:26:53 -0400

    Update producer.c

--- C/producer.c
+++ C/producer.c
@@ -16,16 +16,16 @@
 #include <stdio.h>
 #include <string.h>
 
-#define SIZE 2
+#define SIZE 2		// only 2 items in the buffer
 #define SHM_KEY 0x1234
 
 struct buffer {
-	int id;
+	int id;		// for producer/consumer id
 };
 
 struct shmbuf {
-	int sem_1;
-	int sem_2;
+	int sem_1;	// semaphore 1
+	int sem_2;	// semaphore 2
 	struct buffer buf[SIZE];
 };
 
@@ -37,24 +37,24 @@ int main() {
 	
 	struct shmbuf *shmptr;
 	
-	x = shmget(SHM_KEY, sizeof(struct shmbuf), 0644|IPC_CREAT);
-	if (x == -1) printf("shm key error\n");
+	x = shmget(SHM_KEY, sizeof(struct shmbuf), 0644|IPC_CREAT);	// create shm key for shared memory
+	if (x == -1) printf("shm key error\n");	
 	
 	shmptr = shmat(x, NULL, 0);
-	if (shmptr == (void*)-1) printf("shm attach error\n");
+	if (shmptr == (void*)-1) printf("shm attach error\n");		// attach shared memory
 	
-	struct buffer produce_next;
+	struct buffer produce_next;					// produce using for loop
 	for (int i = 0; i < 10; i++) {
-		while((shmptr->sem_1 + 1) % SIZE == shmptr->sem_2) {
-			printf("Producer started producing data.\n");
+		while((shmptr->sem_1 + 1) % SIZE == shmptr->sem_2) {	// while there is a slot open...
+			printf("Producer started producing data.\n");	// produce something
 			sleep(1); 
 			continue;
 		}
-	produce_next.id = shmptr->sem_1;
-	shmptr->buf[shmptr->sem_1] = produce_next;
-	shmptr->sem_1 = (shmptr->sem_1 + 1) % SIZE;
+	produce_next.id = shmptr->sem_1;				// prod next = semaphore1
+	shmptr->buf[shmptr->sem_1] = produce_next;			// buffer[semaphore 1] = next
+	shmptr->sem_1 = (shmptr->sem_1 + 1) % SIZE;			// semaphore 1 = next % size
 	
-	printf("Producer id: %d\n", produce_next.id);
+	printf("Producer id: %d\n", produce_next.id);			// show producer id
 	
 	}
 	
