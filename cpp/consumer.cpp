0231151dc70c1990c46f5820e0c645c72b27f66b
Nassim Marhari <92116345+nmarhari@users.noreply.github.com>
2023-04-03 21:34:32 -0400

    Update consumer.cpp

--- cpp/consumer.cpp
+++ cpp/consumer.cpp
@@ -13,6 +13,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
 const int SIZE = 2;
 
 struct shmbuf {
@@ -24,14 +26,16 @@ struct shmbuf {
 int main() {
 
 	int fd = shm_open("table", O_RDONLY, 0666);
+	if (fd == -1) { perror("error creating memory"); exit(EXIT_FAILURE); }
 	
 	struct shmbuf *shmp = (shmbuf*)mmap(NULL, sizeof(*shmp), PROT_READ, MAP_SHARED, fd, 0);
+	if (shmp == MAP_FAILED) { errExit("map failed"); }
 	printf("Consumer mapped address: %p\n", shmp);
 	
-	sem_wait(&shmp->sem_1);
-	printf("semwait sem_1 consumer\n");
-	printf("consumer started consuming data\n");
 	for (int i = 0; i < SIZE; i++) {
+		printf("semwait sem_1 consumer\n");
+		if (sem_wait(&shmp->sem_1) == -1) errExit("sem_wait");
+		printf("consumer started consuming data\n");
 		printf("Accessed shared memory buffer.\n");
 		printf("Shared memory in buf[%d] is: ", i);
 		printf("%d", (shmp->buf[i]));
