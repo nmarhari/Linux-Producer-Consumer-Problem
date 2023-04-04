9950993ac6145725a2a7f8e93c3b22349172d7ee
Nassim Marhari <92116345+nmarhari@users.noreply.github.com>
2023-04-03 22:52:40 -0400

    Update consumer.cpp

--- cpp/consumer.cpp
+++ cpp/consumer.cpp
@@ -13,47 +13,42 @@
 #include <stdio.h>
 #include <string.h>
 
+#define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
 const int SIZE = 2;
-const int SPACE = (SIZE * sizeof(int));
+
+struct shmbuf {
+	sem_t sem_1;
+	sem_t sem_2;
+	int buf[SIZE];
+};
 
 int main() {
 
-	sem_t *mutex = sem_open("mutex", O_CREAT, 0666, 1);
-	if (mutex == SEM_FAILED) { perror("error creating mutex"); exit(EXIT_FAILURE); }
-	sem_t *full = sem_open("full", O_CREAT,0666, 0);
-	if (full == SEM_FAILED) { perror("error creating full"); exit(EXIT_FAILURE); }
-	sem_t *empty = sem_open("empty", O_CREAT, 0666, SIZE);
-	if (empty == SEM_FAILED) { perror("error creating empty"); exit(EXIT_FAILURE); }
-
-	int shm = shm_open("table", O_RDONLY, 0666);
-
+	int fd = shm_open("table", O_RDONLY, 0666);
+	if (fd == -1) { perror("error creating memory"); exit(EXIT_FAILURE); }
 	
-	int *tbl = (int*)mmap(0, SIZE, PROT_READ, MAP_SHARED, shm, 0);
-	printf("Consumer mapped address: %p\n", tbl);
+	struct shmbuf *shmp = (shmbuf*)mmap(NULL, sizeof(*shmp), PROT_READ, MAP_SHARED, fd, 0);
+	if (shmp == MAP_FAILED) { errExit("map failed"); }
+	printf("Consumer mapped address: %p\n", shmp);
 	
-	printf("consumer started consuming data\n");
 	for (int i = 0; i < SIZE; i++) {
-		if(sem_wait(full) == -1) { perror("error waiting on full"); exit(EXIT_FAILURE); }
-		if(sem_wait(mutex) == -1) { perror("error waiting on mutex"); exit(EXIT_FAILURE); }
-		
-		printf("inside for loop cons");
-		printf("Consumer: %d\n", tbl[i]);
-		sem_post(mutex);
-		sem_post(empty);
+		printf("semwait sem_1 consumer\n");
+		if (sem_wait(&shmp->sem_1) == -1) errExit("sem_wait");
+		printf("consumer started consuming data\n");
+		printf("Accessed shared memory buffer.\n");
+		printf("Shared memory in buf[%d] is: ", i);
+		printf("%d", (shmp->buf[i]));
 	}
-		
 	
-	munmap(tbl, SPACE);
-	close(shm);
-
+	sem_post(&shmp->sem_2);
+	printf("sempost sem_2 consumer\n");
 	
-	sem_close(mutex);
-	sem_close(full);
-	sem_close(empty);
+	munmap(shmp, SIZE);
+	close(fd);
 	
-	sem_unlink("mutex");
-	sem_unlink("full");
-	sem_unlink("empty");
+	sem_unlink("sem_1");
+	sem_unlink("sem_2");
 
 	printf("Consumer exiting.\n");
 	
