5cf1ca55e09a3c0b0e6a3ec77da05e01ee7ea40e
Nassim Marhari <92116345+nmarhari@users.noreply.github.com>
2023-04-06 19:00:23 -0400

    Create while_loop_consumer.c

---
+++ C/while_loop_consumer.c
@@ -0,0 +1,80 @@
+// Nassim Marhari
+// Linux Producer Consumer problem
+// consumer.c
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 2		// only 2 items in the table
+#define SHM_KEY 0x1234
+
+struct shmbuf {
+	sem_t mutex;	// semaphore 1
+	sem_t empty;	// semaphore 2
+	int table[SIZE];
+};
+
+void* consumer_thread(void* arg) {
+	
+	printf("consumer thread created\n");
+	
+	time_t t;
+	srand((unsigned) time(&t));
+	int shm;
+	
+	struct shmbuf *shmptr;
+	
+	shm = shmget(SHM_KEY, sizeof(struct shmbuf), 0644|IPC_CREAT);
+	if (shm == -1) printf("shm key error\n");
+	
+	shmptr = shmat(shm, NULL, 0);
+	if (shm == -1) printf("shm attach error\n");
+	
+	sleep(1);
+	
+	sem_wait(&shmptr->mutex);
+	printf("Consumer entered...\n");
+	
+	int i, j = 1;
+	sem_getvalue(&shmptr->empty, &i);
+	while (i < SIZE) {
+		
+		int x;
+		x = shmptr->table[j];
+		sem_post(&shmptr->empty);
+		printf("Consumed item %d\n", j);
+		printf("Item %d consumed = ", j);
+		printf("%d\n", x);
+		sem_getvalue(&shmptr->empty, &i);
+		++j;
+		
+	}
+
+	printf("Consumer done consuming items since the table is empty.\n");
+
+	sem_post(&shmptr->mutex);
+
+}
+
+int main() {
+
+
+	pthread_t consumer;
+	pthread_create (&consumer, NULL, consumer_thread, NULL);
+	
+	pthread_join(consumer, NULL);
+
+	return 0;
+}
