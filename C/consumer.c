db23ce0f17f29e4ef5ecd50a5ad811531a4b63e0
Nassim Marhari <92116345+nmarhari@users.noreply.github.com>
2023-04-06 19:27:44 -0400

    Update consumer.c

--- C/consumer.c
+++ C/consumer.c
@@ -1,5 +1,6 @@
 // Nassim Marhari
 // Linux Producer Consumer problem
+// consumer.c
 
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -16,47 +17,70 @@
 #include <stdio.h>
 #include <string.h>
 
-#define SIZE 2		// only 2 items in the buffer
+#define SIZE 2		// only 2 items in the table
 #define SHM_KEY 0x1234
-
-struct buffer {
-	int id;		// for producer/consumer id
-};
+#define ITERATIONS 10
 
 struct shmbuf {
-	int sem_1;	// semaphore 1
-	int sem_2;	// semaphore 2
-	struct buffer buf[SIZE];
+	sem_t mutex;	// semaphore 1
+	sem_t empty;	// semaphore 2
+	int table[SIZE];
 };
+
+void* consumer_thread(void* arg) {
+	
+	printf("Consumer thread created.\n");
+	
+	time_t t;
+	srand((unsigned) time(&t));
+	int shm;
+	
+	struct shmbuf *shmptr;
+	
+	shm = shmget(SHM_KEY, sizeof(struct shmbuf), 0644|IPC_CREAT);
+	if (shm == -1) printf("SHM key error\n");
+	
+	shmptr = shmat(shm, NULL, 0);
+	if (shm == -1) printf("SHM attach error\n");
+	
+	sleep(1);
+	
+	for (int it = 0; it < ITERATIONS; it++) {
+	
+		sleep(1);
+		sem_wait(&shmptr->mutex);
+		printf("Consumer entered...\n");
+		
+		int i, j = 1;
+		sem_getvalue(&shmptr->empty, &i);
+		while (i < SIZE) {
+			
+			int x;
+			x = shmptr->table[j];
+			sem_post(&shmptr->empty);
+			printf("Consumed item %d\n", j);
+			printf("Item %d consumed = ", j);
+			printf("%d\n", x);
+			++j;
+			sem_getvalue(&shmptr->empty, &i);
+			
+		}
+
+		printf("Consumer done consuming items since the table is empty.\n");
+
+		sem_post(&shmptr->mutex);
+	}
+	
+	printf("Consumer thread has finished working.\n");
+}
 
 int main() {
 
-	time_t t;
-	srand((unsigned) time(&t));
-	int x, y;
+
+	pthread_t consumer;
+	pthread_create (&consumer, NULL, consumer_thread, NULL);
 	
-	struct shmbuf *shmptr;	// struct pointer
-	
-	x = shmget(SHM_KEY, sizeof(struct shmbuf), 0644|IPC_CREAT);	// create shm key for shared memory
-	if (x == -1) printf("shm key error\n");
-	
-	shmptr = shmat(x, NULL, 0);					// attach shared memory
-	if (shmptr == (void*)-1) printf("shm attach error\n");
-	
-	struct buffer consume_next;					// consume using for loop
-	for (int i = 0; i < 10; i++) {
-		while(shmptr->sem_1 == shmptr->sem_2) {			// while semaphore 1 and 2 are the same...
-			printf("Consumer started consuming data.\n");	// consume
-			sleep(1); 
-			continue;
-		}
-		
-	consume_next = shmptr->buf[shmptr->sem_2];			// next = buffer[semaphore 2]
-	shmptr->sem_2 = (shmptr->sem_2 + 1) % SIZE;			// semaphore 2 = next % size
-	sleep(1);
-	printf("Consumer id: %d\n", consume_next.id);			// show consumer id
-	}
-	
+	pthread_join(consumer, NULL);
 
 	return 0;
 }
